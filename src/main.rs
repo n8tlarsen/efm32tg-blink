c0f42d3238b093fa9f81deb36a70b3273aafa567
Nathan <n8tlarsen@gmail.com>
2023-02-27 09:42:24 -0600

    minimal reproducible issue

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,58 @@
 #![no_main]
 #![no_std]
 
-use core::panic::PanicInfo;
+#[rtic::app(device = efm32tg11b_pac::efm32tg11b120, dispatchers = [CSEN])]
+mod app {
 
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m_rt::{entry, exception};
-use efm32gg_pac::efm32gg990::gpio;
+    use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
-}
-
-static mut COUNT: u32 = 0;
-
-#[allow(non_snake_case)]
-#[exception]
-fn SysTick() {
-    unsafe {
-        COUNT = COUNT.wrapping_add(1);
+    #[panic_handler]
+    fn panic(_panic: &PanicInfo<'_>) -> ! {
+        loop {}
     }
-}
+    
+    // Shared resources go here
+    #[shared]
+    struct Shared {
+        // TODO: Add resources
+    }
 
-fn delay(ticks_ms: u32) {
-    unsafe {
-        let end = COUNT.wrapping_add(ticks_ms);
+    // Local resources go here
+    #[local]
+    struct Local {
+        // TODO: Add resources
+    }
 
-        while end > COUNT {
-            cortex_m::asm::wfi();
+    #[init]
+    fn init(_cx: init::Context) -> (Shared, Local, init::Monotonics) {
+
+        task1::spawn().ok();
+
+        // Setup the monotonic timer
+        (
+            Shared {
+                // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+            },
+            init::Monotonics(
+                // Initialization of optional monotonic timers go here
+            ),
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            continue;
         }
     }
-}
 
-fn init_systick(cortex_periph: &mut cortex_m::Peripherals) {
-    let syst = &mut cortex_periph.SYST;
-    const DEFAULT_HZ: u32 = 14_000_000u32;
+    // TODO: Add tasks
+    #[task]
+    fn task1(_cx: task1::Context) {
 
-    syst.set_clock_source(SystClkSource::Core);
-    syst.set_reload(DEFAULT_HZ / 1_000u32);
-    syst.clear_current();
-    syst.enable_counter();
-    syst.enable_interrupt();
-}
-
-#[entry]
-fn main() -> ! {
-    let mut cortex_periph = cortex_m::Peripherals::take().unwrap();
-    // PE2 = LED0
-    // PE3 = LED1
-    init_systick(&mut cortex_periph);
-    let efm32 = efm32gg_pac::efm32gg990::Peripherals::take().unwrap();
-    // enable GPIO clock
-    efm32.CMU.hfperclken0.write(|w_reg| w_reg.gpio().set_bit());
-    efm32.GPIO.pe_model.write(|w_reg| {
-        w_reg
-            .mode3()
-            .variant(gpio::pe_model::MODE3_A::PUSHPULL)
-            .mode2()
-            .variant(gpio::pe_model::MODE2_A::PUSHPULL)
-    });
-    efm32
-        .GPIO
-        .pe_doutset
-        .write(|w_reg| w_reg.doutset().variant(1 << 2));
-    efm32
-        .GPIO
-        .pe_doutclr
-        .write(|w_reg| w_reg.doutclr().variant(1 << 3));
-
-    let mut is_set = true;
-
-    loop {
-        delay(250);
-
-        if !is_set {
-            efm32
-                .GPIO
-                .pe_doutclr
-                .write(|w_reg| w_reg.doutclr().variant(1 << 3));
-            efm32
-                .GPIO
-                .pe_doutset
-                .write(|w_reg| w_reg.doutset().variant(1 << 2));
-        } else {
-            efm32
-                .GPIO
-                .pe_doutclr
-                .write(|w_reg| w_reg.doutclr().variant(1 << 2));
-            efm32
-                .GPIO
-                .pe_doutset
-                .write(|w_reg| w_reg.doutset().variant(1 << 3));
-        }
-
-        is_set = !is_set
     }
 }
